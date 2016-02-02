9e2f45c0085c3632d0371ada371a4c4dda3575f9
Matt Hays <mdhays1995@gmail.com>
2016-02-02 11:28:46 -0600

    Express is now installed and being used.

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
 'use strict';
 
-const http = require('http');
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  console.log(req.method, req.url);
-  res.writeHead(200, {
-    'Content-type': 'text/html'
-  });
-  if(req.url === '/hello') {
-    const msgInABottle = '<h1>Hello World!</h1>';
+app.get('/hello', (req, res) => {
+
+    const msgInABottle = '<h1>Hello World!!!!</h1>';
+    
+    console.log(req.method, req.url);
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
 
     msgInABottle.split('').forEach((char, i) => {
+      const events = [];
       setTimeout(() => {
         res.write(char);
+        events.pop();
+        console.log(char);
     }, 1000 * i);
+      events.push(i)
   });
 
   setTimeout(() => {
     res.end();
-  }, 20000);
+  }, 25000);
+});
 
-  } else if (req.url === '/random') {
+app.get('/random', (req, res) => {
     res.end(Math.random().toString());
-  } else {
-    res.writeHead(403);
-    res.end('Access Denied');
-  }
-}).listen(PORT, () => {
+});
+
+app.all('*', (req, res) => {
+  res.status(403);
+  res.send('<h1>access denied</h1>');
+});
+
+app.listen(PORT, () => {
   console.log(`Node.js server started. Listening on ${ PORT }.`);
 });
