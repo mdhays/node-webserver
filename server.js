c8482b5d006c0478682e40fdea16c44e1f360949
Matt Hays <mdhays1995@gmail.com>
2016-02-03 10:43:12 -0600

    Used Jade in example.

--- server.js
+++ server.js
@@ -3,16 +3,23 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.set('view engine', 'jade');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+      title: 'Super Cool App',
+      date: new Date()
+  });
+});
 
 app.get('/hello', (req, res) => {
 
     const msgInABottle = '<h1>Hello World!!!!</h1>';
-    
     console.log(req.method, req.url);
     res.writeHead(200, {
       'Content-type': 'text/html'
     });
-
+  console.log(req.query);
     msgInABottle.split('').forEach((char, i) => {
       const events = [];
       setTimeout(() => {
@@ -28,9 +35,16 @@ app.get('/hello', (req, res) => {
   }, 25000);
 });
 
-app.get('/random', (req, res) => {
-    res.end(Math.random().toString());
+app.get('/random:min/:max', (req, res) => {
+  const min = req.params.min;
+  const max = req.params.max;
+
+  res.send(getRandomInt(+min, +max).toString());
 });
+
+// app.get('/cal') => {
+
+// }
 
 app.all('*', (req, res) => {
   res.status(403);
@@ -40,3 +54,8 @@ app.all('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Node.js server started. Listening on ${ PORT }.`);
 });
+
+
+// Child process exec
+// Use npm to install cal to this directory.
+// Interface directly with cal
