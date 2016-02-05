d566280c6be049c99369c0560b789eb1302d5104
Matt Hays <mdhays1995@gmail.com>
2016-02-05 11:45:14 -0600

    Pulling from CNN to grab headline titles and urls.

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const bodyParser = require('body-parser');
 //const upload = require('multer')({dest: 'tmp/uploads'});
 const multer = require('multer');
+const request = require('request');
+const _ = require('lodash');
+const cheerio = require('cheerio');
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'tmp/uploads')
@@ -16,16 +19,68 @@ var storage = multer.diskStorage({
     }
   })
 
-  var upload = multer({ storage: storage })
+var upload = multer({ storage: storage })
 const PORT = process.env.PORT || 3000;
 app.set('view engine', 'jade');
 
 // app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index', {
       title: 'Super Cool App',
       date: new Date()
+  });
+});
+// Sets a header and sends a json object to the server.
+// Setting the header to 'Access-Control-Allow-Origin' allows the browser to access the page/data.
+app.get('/api', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.send({what: 'up'});
+});
+
+app.post('/api', (req, res) => {
+  console.log('please mister postman');
+  const obj = _.mapValues(req.body, val => val.toUpperCase());
+  res.send(obj);
+  console.log(req.body);
+});
+
+// Set up to demonstrate what was happening in our ionic project where our requests were through a proxy
+// Gets the weather using forecast.io
+app.get('/api/weather', (req, res) => {
+  const apiKey = 'da186af328648fe7e2f94de9f6998569';
+  const url = `https://api.forecast.io/forecast/${apiKey} /37.8267,-122.423`;
+  request.get(url, (err, response, body) => {
+    if (err) throw err;
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.send(JSON.parse(body));
+  });
+});
+
+app.get('/api/news', (req, res) => {
+  const url = 'http://cnn.com';
+  
+  request.get(url, (err, response, body) => {
+    if (err) throw err;
+
+    const news = [];
+    const $ = cheerio.load(body);
+
+    news.push({
+      title: $('.banner-text').text(),
+      url: $('.banner-text').closest('a').attr('href')
+    });
+
+    _.range(1, 12).forEach(i => {
+        news.push({
+        'title': $('.cd__headline').eq(i).text(),
+        'url': $('.cd__headline').eq(i).find('a').attr('href')
+      });
+    });
+
+    res.send(news);
   });
 });
 
